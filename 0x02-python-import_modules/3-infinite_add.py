43f994a42928f2bcd2745ff88e0c43bd17839d61
solomonenanaw <sekewbeg@gmail.com>
2022-06-30 18:01:30 +0000

    syntax

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,13 +7,4 @@ if __name__ == "__main__":
     result = 0
     for i in range(len(sys.argv) - 1):
         result += int(sys.argv[i + 1])
-    print("{}".format(result))#!/usr/bin/python3
-
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-
-    result = 0
-    for i in range(len(sys.argv) - 1):
-        result += int(sys.argv[i + 1])
     print("{}".format(result))
