894c23f6addc4d4c82c1513bfb1d6be7d06fe328
solomonenanaw <sekewbeg@gmail.com>
2022-07-05 01:14:03 -0700

    f

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print()
+       print()
     else:
         for row in range(len(matrix)):
             for item in range(len(matrix[row])):
@@ -10,4 +10,5 @@ def print_matrix_integer(matrix=[[]]):
                 else:
                    endspace = ''
                 print("{:d}".format(matrix[row][item]), end=endspace)
-            print()
+             print()
+ 
