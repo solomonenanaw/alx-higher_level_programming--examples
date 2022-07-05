2934e49e8bdcd7510444d8d5766c6bec97955725
solomonenanaw <sekewbeg@gmail.com>
2022-07-05 02:37:09 -0700

    f

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,5 +5,5 @@ def max_integer(my_list=[]):
             if my_list[0] > my_list[1]:
                 return max_integer(my_list[:1] + my_list[2:])
             return max_integer(my_list[1:])
-         return my_list[0]
+        return my_list[0]
     return None
